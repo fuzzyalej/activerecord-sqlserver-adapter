8b39acef72bb7935f48e75195abee7cb3f8d1d4f
Alejandro Andres <fuzzy.alej@gmail.com>
2014-11-17 12:56:01 +0100

    temporal patch to use v.2014

--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -65,7 +65,7 @@ module ActiveRecord
                            if @database_version =~ /Azure/i
                              @sqlserver_azure = true
                              @database_version.match(/\s-\s([0-9.]+)/)[1]
-                             year = 2012
+                             year = 2014
                            else
                              year = DATABASE_VERSION_REGEXP.match(@database_version)[1]
                              year == 'Denali' ? 2011 : year.to_i
